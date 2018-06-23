6d840027f134f49f9e438973d23045e6c3a5b48c
Valerie Fuchs <valeriefuchs@Valeries-MacBook-Pro.local>
2018-06-23 15:55:26 +0200

    Added some small animations

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let screen01
 let screen02
 let screen03
+let nextMeetup
+let date
+let eventLocation
+let introText
 
 function elementIsVisible(element){
     let vh = window.innerHeight
@@ -9,10 +13,31 @@ function elementIsVisible(element){
       && !(bounds.top > vh && bounds.bottom > vh)
 }
 
+// function addAnimation(element, direction){
+//     element.style.transform = "translate(10px)"
+// }
+
 window.addEventListener('load', function(){ 
     screen01 = document.getElementById('screen-01')
     screen02 = document.getElementById('screen-02')
     screen03 = document.getElementById('screen-03')
+
+    nextMeetup = document.querySelector('.next-meetup')
+    date = document.querySelector('.date')
+    eventLocation = document.querySelector('.location')
+
+    introText = document.querySelector('.full-size')
+    console.log('INTRO TEXT:')
+    console.log(introText)
+
+    introText.addEventListener('click', function(){
+        nextMeetup.innerText = "MEXT MEETUP"
+        nextMeetup.style.color = "#9013FE"
+        eventLocation.innerText = "DE RUYSCHKAMER"
+        eventLocation.style.color = "#9013FE"
+        date.innerText = "JUNE 27TH"
+        date.style.color = "#9013FE"
+    })
 }) 
 
 window.addEventListener('scroll', function() {
